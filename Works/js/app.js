4ff6a074f858a04b5b3fd1c20bc0786bdd940680
Zohrab Semedzade <zohrabsemedzade3@gmail.com>
2021-09-01 00:49:26 +0400

    Week05Day02 lesson

---
+++ Works/js/app.js
@@ -0,0 +1,34 @@
+/* let yil = prompt('Bu saytin qurucusu kimdir', '');
+
+if (yil == "zohrab") {
+    alert( 'halaldi sene oprem!' );
+}else{
+    alert( 'ncncncnc bir mene bax gorum kimem men!' );
+} */
+/* let ad = prompt('Zohrab ne vaxt dunyaya gelib?')
+
+if (ad < 2003){
+    alert('e yo o qeder qoca doulem!')
+}else if (ad > 2003) {
+    alert('e yo o qeder balaca doulem!')
+}else {
+    alert('ay yaşa!')
+} */
+/* let a = prompt('nece yasin var?')
+
+if (a < 3){
+    alert('ae balaca burda ne işin var!')
+}else{
+    alert('sen tek canavarsan')
+}    */
+
+/* let a = 1;
+while(a <= 6){
+    alert(a);
+    ++a;
+} */
+function Foo() {
+    return a+b+c+d+e+f;
+}
+/* let cavab = Foo(3, 4, 12, 45, 67, 78) */
+alert (cavab)
---
+++ Works/js/index.js
@@ -0,0 +1,56 @@
+//alert("This is my Portfolio");
+//var learning
+/* 
+var age = (18)
+    var firstName = "Zohrab";
+    var lastName = "Semedzade";
+    var online = true;
+    var whatever = null;
+    age = age + 1;
+    var fullName = firstName +" "+ lastName 
+*/
+
+/*
+    whatever = "Code" 
+*/
+//console learning
+/* 
+    console.log("This is my Portfolio");
+    console.log("My age is" ,age)
+    console.log("My name is" ,fullName)
+    console.log("Are you online?:",online)
+    console.log(whatever) 
+*/
+
+/* 
+    var friends = 10; 
+*/
+
+
+/*  math
+    friends = friends + 1;
+    friends = friends *3
+    friends = friends /3;
+    friends = friends **2 
+    friends = 10 + 2 / 2; 
+    friends = (10+2) / 2;
+*/
+
+
+/* 
+    shortcuts
+    friends += 1
+    friends -=2
+    friends *= 3
+    friends **=2 
+*/
+
+/*
+    let a="salam";
+    console.log(typeof a); 
+*/
+/* var a = 5 */
+/* a = 6 */
+/* a=5; b=6
+a=b
+console.log(a) */
