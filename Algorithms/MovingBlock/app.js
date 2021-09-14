565132ef9e08a683971b5f9ea9439fb32f1f817f
Zohrab Semedzade <zohrabsemedzade3@gmail.com>
2021-09-14 22:51:45 +0400

    Making moving block

---
+++ Algorithms/MovingBlock/app.js
@@ -0,0 +1,42 @@
+/* let btn = document.createElement('button');
+btn.innerHTML = 'click';
+document.querySelector('.box').appendChild(btn);
+
+btn.addEventListener('click', function() {
+    document.querySelector('.box').style.backgroundColor='red';
+}); */
+/* let btn = document.createElement('button');
+btn.innerHTML = 'click';
+document.body.appendChild(btn);
+let marginTopu = 0
+btn.addEventListener('click', function() {
+    document.querySelector('.box')
+    marginTopu+=10
+    box.style.top=`${marginTopu}px`
+}); */
+let box =document.getElementById('box');
+let container=document.getElementById('container')
+let up2=1
+let left2=1
+let right2=1
+let down2=1
+function up(){
+    box.style.height=30 + 'px';
+    box.style.bottom=30 * up2 + 'px';
+    up2++;
+}
+function left(){
+    box.style.left=30 + 'px';
+    box.style.right=30 * left2 + 'px';
+    left2++;
+}
+function right(){
+    box.style.right=30 + 'px';
+    box.style.left=30 * right2 + 'px';
+    right2++;
+}
+function down(){
+    box.style.bottom=30 + 'px';
+    box.style.height=30 * down2 + 'px';
+    down2++;
+}
---
+++ Algorithms/slide.js
@@ -0,0 +1,5 @@
+function deleteitem(elem){
+    let elem = document.createElement("image")
+/*     elem.style.width='300px';
+    elem.style.background='blue' */
+}
