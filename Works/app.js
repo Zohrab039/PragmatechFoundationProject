a226833b82a28dd4fccd9a54d07108c185eace87
Zohrab Semedzade <zohrabsemedzade3@gmail.com>
2021-10-12 21:34:08 +0400

    adding new things

--- Works/app.js
+++ Works/app.js
@@ -1,4 +1,4 @@
-let btn=document.createElement('button')
+/* let btn=document.createElement('button')
 btn.innerHTML='Click Me'
 let box=document.querySelector('.box')
 
@@ -15,4 +15,25 @@ btn.addEventListener('click',function(){
     box.style.height=`${baslangicHundurluk}px`
     box.style.background=`rgb(${randR},${randG},${randB})`
     console.log(randR,randG,randB)
-})
+}) */
+
+/* a='Hello World'; */
+/* let a;
+let b;
+let cem;
+a=5;
+b=6;
+cem=a+b;
+console.log(cem); */
+
+let a;
+a=5;
+console.log(a)
+a=4;
+console.log(a)
+let b;
+b=-1;
+b=34;
+let sum;
+sum=a*b
+console.log(sum)
