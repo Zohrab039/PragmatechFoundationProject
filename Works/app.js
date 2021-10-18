cf150f37ae90a7e53edbbd029e16fa5390f93952
Zohrab Semedzade <zohrabsemedzade3@gmail.com>
2021-10-18 23:26:36 +0400

    tasks done

--- Works/app.js
+++ Works/app.js
@@ -3,14 +3,12 @@ btn.innerHTML='Click Me'
 let box=document.querySelector('.box')
 
 box.appendChild(btn)
-let baslangicGenislik=200
-let baslangicHundurluk=300
+let baslangicGenislik=1325
+let baslangicHundurluk=668
 btn.addEventListener('click',function(){
     let randR=Math.floor(Math.random()*255)
     let randG=Math.floor(Math.random()*255)
     let randB=Math.floor(Math.random()*255)
-    baslangicGenislik+=30
-    baslangicHundurluk+=30
     box.style.width=`${baslangicGenislik}px`
     box.style.height=`${baslangicHundurluk}px`
     box.style.background=`rgb(${randR},${randG},${randB})`
@@ -46,3 +44,87 @@ console.log(sum) */
 // data structures
 // conditions
 // loops
+/* let a = 5
+console.log(a++)
+console.log(a) */
+/* console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log(6)
+console.log(7)
+console.log(8)
+console.log(9)
+console.log(10) */
+/* let a=1; */
+/* function Artir(){
+    console.log(a);
+    a=a+1;
+    if(a<10){
+        Artir()
+    }
+}
+Artir() */
+
+/* while(a<100){
+    console.log(a);
+    a++
+} */
+
+/* for(let i=1; i<300; i++){
+    console.log(i)
+} */
+
+//Task #1 start
+/* let a=(3+4*2==2*3)&&(true+false/2)
+//let a=(3+8==6)&&(true+0)
+//let a=(11==6)&&1
+//let a=false&&1
+//let a=false
+console.log(a) */
+//Task #1 done
+
+//Task #2 start
+/* let a = 1298;
+let sum = 0;
+while (a > 0) {
+    let son = a % 10;
+    sum =sum+ son;
+    a = parseInt(a/10);
+}
+console.log(sum) */
+//Task #2 done
+
+//Task #3 start
+/* let a=1298
+let str=a.toString()
+function odd(str){
+    if(str%2==1){
+    }console.log(odd)
+} */
+//Task #3 not done
+
+//Task #4 start
+let username= prompt("What is your name?");
+for(let i=1; i<11; i++){
+    console.log(username)
+}
+//Task #4 done
+
+//Task #5 start
+let enterAge=prompt("What is your age? ");
+if(enterAge<=30){
+    console.log(enterAge*enterAge)
+}/* else if(enterAge<40){
+
+} */else if(enterAge>100)(enterAge<0);{
+    console.log('The information you entered is incorrect')
+}
+
+
+
+
+
+
+
